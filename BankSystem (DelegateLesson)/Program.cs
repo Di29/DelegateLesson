04908634ca25c63f80f2cbd645ef1ef5052aa304
Urinbasarov Dias <jentle.men29@gmail.com>
2018-10-24 18:14:19 +0600

    lesson

--- BankSystem (DelegateLesson)/Program.cs
+++ BankSystem (DelegateLesson)/Program.cs
@@ -11,16 +11,46 @@ namespace BankSystem__DelegateLesson_
         static void Main(string[] args)
         {
             Account account = new Account();
-            account.ActionEvent += new Messenger().SendMessenger;
+
+            //account.ActionEvent += new Messenger().SendMessenger;
+            account.ActionEvent += delegate (string message)
+            {
+                Console.WriteLine(message);
+            };
+
+            account.ActionEvent += (message) => Console.WriteLine(message);
+            //тоже самое
 
             account.Add(1000);
             account.Withdraw(500);
+
+            List<Account> accounts = new List<Account>
+            {
+                new Account(),
+                new Account(),
+                new Account(),
+                new Account(),
+                new Account(),
+            };
+
+            List<Account> resultAccount = new List<Account>();
+            foreach(var acc in accounts)
+            {
+                if(acc.Sum >= 500)
+                {
+                    resultAccount.Add(acc);
+                }
+            }
+
+            var result = accounts.Where(acc => acc.Sum >= 500);
+            var result2 = from acc
+                          in accounts
+                          where acc.Sum >= 500
+                          select acc;
+
             Console.ReadLine();
         }
 
-        //static void SendMessage(string message)
-        //{
-        //    Console.WriteLine(message);
-        //}
+        
     }
 }
